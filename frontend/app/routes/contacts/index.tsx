12ea083ac2b6eff863c8dd5c3d5788f09466cee3
Evgeniy Budaev <budaev.e@gmail.com>
2022-06-04 10:06:13 +0300

    install Mock Service Worker

--- frontend/app/routes/contacts/index.tsx
+++ frontend/app/routes/contacts/index.tsx
@@ -1,4 +1,18 @@
+import type {LoaderFunction} from "@remix-run/node";
+import {useLoaderData} from "@remix-run/react";
+import * as React from 'react';
+
+export const loader: LoaderFunction = async () => {
+	console.log("START");
+	const res =  await fetch('/contacts').then((res) => res.json());
+	console.log("RESULT: ", res);
+	return res;
+}
+
 function Index() {
+	const data = useLoaderData();
+	console.log("DATA: ", data);
+
 	return (
 		<section>
 			<h1>Контакты</h1>
--- frontend/mocks/index.js
+++ frontend/mocks/index.js
@@ -1,3 +1,11 @@
 require("tsconfig-paths/register");
 require("ts-node").register({ transpileOnly: true });
 require("./start");
+
+// if (typeof window === 'undefined') {
+// 	const { server } = require('./server');
+// 	server.listen();
+// } else {
+// 	const { worker } = require('./browser');
+// 	worker.start();
+// }
