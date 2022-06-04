aa6a859342982f2e5f52bfa13e7d92ef337c5fb6
Evgeniy Budaev <budaev.e@gmail.com>
2022-06-04 10:13:50 +0300

    /contacts

--- frontend/app/mocks/handlers.js
+++ frontend/app/mocks/handlers.js
@@ -1,7 +1,7 @@
 import { rest } from 'msw';
 
 export const handlers = [
-	rest.get('http://localhost:3000/contacts', (req, res, ctx) => {
+	rest.get('/contacts', (req, res, ctx) => {
 		return res(
 			ctx.json({
 				id: 'f79e82e8-c34a-4dc7-a49e-9fadc0979fda',
