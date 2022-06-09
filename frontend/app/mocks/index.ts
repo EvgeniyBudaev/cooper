4b842d572f4dc09a8514a0d35ab21b3a669495f6
Evgeniy Budaev <budaev.e@gmail.com>
2022-06-09 16:03:58 +0300

    server.close

--- frontend/app/mocks/index.ts
+++ frontend/app/mocks/index.ts
@@ -2,6 +2,8 @@ export const init = ():void => {
 	if (typeof window === 'undefined') {
 		const { server } = require('./server')
 		server.listen({ onUnhandledRequest: "bypass" });
+		process.once("SIGINT", () => server.close());
+		process.once("SIGTERM", () => server.close())
 	} else {
 		const { worker } = require('./browser')
 		worker.start({ onUnhandledRequest: "bypass" });
